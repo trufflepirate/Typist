51150be000fb5e59a0e90a4ab2c60def8d32a3b1
trufflepirate <rootfourtytwo@gmail.com>
2023-07-18 17:31:58 +0800

    init audio components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import './App.css';
 import NavigationBar from './components/NavigationBar';
-import { debug_init } from './utilities';
 import { TextWritingArea } from './components/TextWritingArea';
+// import 
+import { debug_init } from './utilities';
 import { StateHandler } from "./StateHandler";
 
 
+function oneTimeLocationCheckbecauseImNotGoingtoUseReactRouter(){
+  const location = window.location.pathname
+  if(location === "/" || location === "/Typist"){
+    const components = (
+      <div>
+        <NavigationBar></NavigationBar>
+        <TextWritingArea/>
+      </div>
+    )
+    return components
+  } else if (location === "/Typist/RecordMaster"){
+    return "kek"
+
+  } else if(location === "/Typist/RemoteRecorder"){
+    return "lmao"
+
+  } else {
+    return "default"
+  }
+}
+
 function App() {
-  return (
-    <div>
-    <NavigationBar ></NavigationBar>
-    <TextWritingArea />
-    </div>
-  );
+  console.log(window.location.pathname)
+  return oneTimeLocationCheckbecauseImNotGoingtoUseReactRouter()
 }
 
 export default App;
---
+++ src/audiocomponents/AudioRecorderReact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AudioRecorderReact = () => {
+    return (
+        <div>
+            <h1>Audio Recorder</h1>
+            <div style={{ height: '200px', backgroundColor: '#f2f2f2' }}>
+                {/* Placeholder space for displaying data */}
+            </div>
+        </div>
+    );
+};
+
+export default AudioRecorderReact;
